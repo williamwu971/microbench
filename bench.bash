5136d2cffdeec7ed5767dd9b40d42c4e05e26606
William <xiwu9085@gmail.com>
2022-09-02 16:45:10 +1000

    -AUTO- Fri Sep  2 04:45:10 PM AEST 2022

--- bench.bash
+++ bench.bash
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 /home/blepers/linux-huge/tools/perf/perf stat -e \
-uncore_imc_0/event=0xE3/,\
-uncore_imc_0/event=0xE3/,\
-uncore_imc_1/event=0xE3/,\
-uncore_imc_1/event=0xE3/,\
-uncore_imc_2/event=0xE3/,\
-uncore_imc_2/event=0xE3/,\
-uncore_imc_3/event=0xE3/,\
-uncore_imc_3/event=0xE3/,\
-uncore_imc_4/event=0xE3/,\
-uncore_imc_4/event=0xE3/,\
-uncore_imc_5/event=0xE3/,\
-uncore_imc_5/event=0xE3/,\
-uncore_imc_6/event=0xE3/,\
-uncore_imc_6/event=0xE3/,\
-uncore_imc_7/event=0xE3/,\
-uncore_imc_7/event=0xE3/ \
+uncore_imc_0/unc_m_pmm_rpq_inserts/,\
+uncore_imc_0/unc_m_pmm_wpq_inserts/,\
+uncore_imc_1/unc_m_pmm_rpq_inserts/,\
+uncore_imc_1/unc_m_pmm_wpq_inserts/,\
+uncore_imc_2/unc_m_pmm_rpq_inserts/,\
+uncore_imc_2/unc_m_pmm_wpq_inserts/,\
+uncore_imc_3/unc_m_pmm_rpq_inserts/,\
+uncore_imc_3/unc_m_pmm_wpq_inserts/,\
+uncore_imc_4/unc_m_pmm_rpq_inserts/,\
+uncore_imc_4/unc_m_pmm_wpq_inserts/,\
+uncore_imc_5/unc_m_pmm_rpq_inserts/,\
+uncore_imc_5/unc_m_pmm_wpq_inserts/,\
+uncore_imc_6/unc_m_pmm_rpq_inserts/,\
+uncore_imc_6/unc_m_pmm_wpq_inserts/,\
+uncore_imc_7/unc_m_pmm_rpq_inserts/,\
+uncore_imc_7/unc_m_pmm_pq_inserts/ \
 $1 $2
